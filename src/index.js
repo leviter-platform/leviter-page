0e47b8d77cde02eafa63876fedaed8fb8d0f808c
Dominic <d151l@d151l.de>
2023-07-07 18:47:26 +0200

    rename PlatformCreateStepTow

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import PlatformCreate from "./pages/create/PlatformCreate";
 import Page404 from "./pages/error/Page404";
-import PlatformCreateStapTow from "./pages/create/step/PlatformCreateStapTow";
+import PlatformCreateStepTow from "./pages/create/step/PlatformCreateStepTow";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,7 +14,7 @@ root.render(
         <Routes>
             <Route index element={<App />} />
             <Route path="platforms/create" element={<PlatformCreate />} />
-            <Route path="platforms/create/step/tow" element={<PlatformCreateStapTow />} />
+            <Route path="platforms/create/step/tow" element={<PlatformCreateStepTow />} />
             <Route path="*" element={<Page404 />} />
         </Routes>
     </BrowserRouter>
